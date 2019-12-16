cfd5a405cf1877171529cde9d856494322b3b2e2
Abdallhss <33943917+Abdallhss@users.noreply.github.com>
2019-12-16 20:09:18 +0900

    Initial commit to include the analysis code, and the tidy data set.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Getting-and-Cleaning-Data-with-R-Coursera-JHU-Data-Science-
+# Getting-and-Cleaning-Data-with-R-Coursera-JHU-Data-Science
+
+This is part of the Getting and Cleaning Data course, one of the courses in the JHU Data science specialization on Coursera.
+In this project, we use the Human Activity Recognition Using Smartphones Data Set from UCI.
+The data is highly clean. It just need to be combined together as requested. The process flow was as follows
+1) First, the train and test data sets are prepared by combining features and labels data frames and renaming if neccessary.
+2) The train and test data were then combined and uncessary columns were dropped to include on data on the mean and std.
+3) A pipe line was created to group the data with the activity and subject and calucate the mean of all features.
+ 
+ 
---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+#download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","Human_Activity_Recognition.zip")
+
+#Read in the features names and activities labels
+features_names <- read.csv("UCI HAR Dataset/features.txt",sep="",header = F)
+activity_labels <- read.csv("UCI HAR Dataset/activity_labels.txt",sep = "",header = F)
+
+#Create a function to read and bind data 
+get_and_combin_data <- function(train = TRUE){
+    # Check if train or test data
+    label <- ifelse(train == T, "train","test")
+    #read the features
+    X <- read.csv(paste("UCI HAR Dataset/",label,"/X_",label,".txt",sep=""),sep="",header = F)
+    # Assign column names
+    names(X) <- features_names[,2]
+    # read activity labels
+    y <- read.csv(paste("UCI HAR Dataset/",label,"/y_",label,".txt",sep=""),header = F)
+    # change numbers to their corresponding activity
+    y $V1= factor(y$V1,levels=activity_labels[,1],labels = activity_labels[,2])
+    # update column name
+    names(y) <- "Activity"
+    # read the subject and update its name
+    subject <- read.csv(paste("UCI HAR Dataset/",label,"/subject_",label,".txt",sep=""),header = F)
+    names(subject) <- "Subject"
+    # bind all columns together
+    cbind(subject,X,y)
+}
+#Get train and test data
+train <- get_and_combin_data(train = TRUE)
+test <- get_and_combin_data(train = FALSE)
+#merge train and test data
+all <- rbind(train,test)
+#Get all names and keep those with mean or std.
+#regular expression is modified not to include meanfrequency
+all_names <- names(all)
+keep_columns = c(grep("(mean|std)[^F]",all_names,value = TRUE),
+                 "Subject","Activity")
+#keep appropiate columns
+#assigned names are appropiate. No need to modify
+all <- all[ , (all_names %in% keep_columns)]
+#check for null values
+sum(is.na(all))
+#check data summary
+summary(all)
+#use dplyr library to ease the data manipulation
+library(dplyr)
+all = tbl_df(all)
+# create a pipe line to group the data and calcualte the mean
+all_mean <- all %>% group_by(Subject,Activity) %>%
+    summarise_all(mean)
+# save the data to csv format
+write.csv(all_mean,file = "Human_Activity_Recognition_Tidy.csv")
